1857fee2f8b0b37875c98f6c8ba2d2571cb50df7
Araceli Carbajal <cbjlara@outlook.com>
2019-10-31 07:42:01 -0600

    Add project files.

---
+++ Biologia/Controllers/HomeController.cs
@@ -0,0 +1,292 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Biologia.Models;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Biologia.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IConfiguration Configuration { get; }
+        public HomeController(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Insectos insecto)
+        {
+            if (ModelState.IsValid)
+            {
+                string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    string f1 = insecto.Fecha.ToString("dd/MM/yyyy");
+                    
+
+                   string sql = $"Insert Into Registro (Usuario,Reino,Phylum,Clase,Orden,Familia,Detalles,Taxa,Morfo,Abun,Frasco,Vegetacion,Metodo,Sustrato,Localida,Fecha,Longitud,Altitud) Values ('{insecto.usuario}','{insecto.reino}','{insecto.phyllum}','{insecto.clase}','{insecto.orden}','{insecto.familia}','{insecto.informacion}','{insecto.Taxa}','{insecto.Morfoespecie}','{insecto.Abundancia}','{insecto.NumFrasco}','{insecto.TipoVegetacion}','{insecto.MetodoColecta}','{insecto.Sustrato}','{insecto.Localidad}',CONVERT(datetime, '{f1}',103),'{insecto.Longitud}','{insecto.Altitud}')";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        
+                        command.CommandType = CommandType.Text;
+                        connection.Open();
+                        Console.Write(insecto.Longitud);
+                        Console.Write(insecto.Altitud);
+                        command.ExecuteNonQuery();
+                        connection.Close();
+                        connection.Dispose();
+                    }
+                    return RedirectToAction("List");
+                }
+            }
+            else
+                return View();
+        }
+        //ver
+        public IActionResult List()
+        {
+            List<Insectos> insecto = new List<Insectos>();
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+               
+                connection.Open();
+
+                string sql = "Select * From Registro"; SqlCommand command = new SqlCommand(sql, connection);
+                using (SqlDataReader dataReader = command.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                    {
+                        Insectos insec = new Insectos();
+                        insec.usuario = Convert.ToString(dataReader["Usuario"]);
+                        insec.Id = Convert.ToInt32(dataReader["Id"]);
+                        //insec.reino = Convert.ToString(dataReader["Reino"]);
+                        //insec.phyllum = Convert.ToString(dataReader["Phylum"]);
+                        //insec.clase = Convert.ToString(dataReader["Clase"]);
+                        insec.orden = Convert.ToString(dataReader["Orden"]);
+                        //insec.familia = Convert.ToString(dataReader["Familia"]);
+                        //insec.informacion = Convert.ToString(dataReader["Detalles"]);
+                        insec.Taxa = Convert.ToString(dataReader["Taxa"]);
+                        insec.Morfoespecie = Convert.ToString(dataReader["Morfo"]);
+                        insec.Abundancia = Convert.ToString(dataReader["Abun"]);
+                        //insec.NumFrasco = Convert.ToInt32(dataReader["Frasco"]);
+                        insec.TipoVegetacion = Convert.ToString(dataReader["Vegetacion"]);
+                        insec.MetodoColecta = Convert.ToString(dataReader["Metodo"]);
+                        insec.Sustrato = Convert.ToString(dataReader["Sustrato"]);
+                        insec.Localidad = Convert.ToString(dataReader["Localida"]);
+                        insec.Fecha = Convert.ToDateTime(dataReader["Fecha"].ToString());
+                        insec.Longitud = Convert.ToString(dataReader["Longitud"]);
+                        insec.Altitud = Convert.ToInt32(dataReader["Altitud"]);
+                        insecto.Add(insec);
+                    }
+                }
+                connection.Close();
+                connection.Dispose();
+            }
+            return View(insecto);
+        }
+
+        public IActionResult Update(int id)
+        {
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            Insectos insec = new Insectos();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"Select * From Registro Where Id='{id}'";
+                SqlCommand command = new SqlCommand(sql, connection);
+                connection.Open();
+                using (SqlDataReader dataReader = command.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                    {
+                        insec.usuario = Convert.ToString(dataReader["Usuario"]);
+                        insec.Id = Convert.ToInt32(dataReader["Id"]);
+                        insec.reino = Convert.ToString(dataReader["Reino"]);
+                        insec.phyllum = Convert.ToString(dataReader["Phylum"]);
+                        insec.clase = Convert.ToString(dataReader["Clase"]);
+                        insec.orden = Convert.ToString(dataReader["Orden"]);
+                        insec.familia = Convert.ToString(dataReader["Familia"]);
+                        insec.informacion = Convert.ToString(dataReader["Detalles"]);
+                        insec.Taxa = Convert.ToString(dataReader["Taxa"]);
+                        insec.Morfoespecie = Convert.ToString(dataReader["Morfo"]);
+                        insec.Abundancia = Convert.ToString(dataReader["Abun"]);
+                        insec.NumFrasco = Convert.ToInt32(dataReader["Frasco"]);
+                        insec.TipoVegetacion = Convert.ToString(dataReader["Vegetacion"]);
+                        insec.MetodoColecta = Convert.ToString(dataReader["Metodo"]);
+                        insec.Sustrato = Convert.ToString(dataReader["Sustrato"]);
+                        insec.Localidad = Convert.ToString(dataReader["Localida"]);
+                        insec.Fecha = Convert.ToDateTime(dataReader["Fecha"].ToString());
+                        insec.Longitud = Convert.ToString(dataReader["Longitud"]);
+                        insec.Altitud = Convert.ToInt32(dataReader["Altitud"]);
+                    }
+                }
+                connection.Close();
+                connection.Dispose();
+            }
+            return View(insec);
+        }
+        [HttpPost]
+        [ActionName("Update")]
+        public IActionResult Update(Insectos insecto)
+        {
+            string f1 = insecto.Fecha.ToString("dd/MM/yyyy");
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"Update Registro SET Usuario='{insecto.usuario}' , Reino='{insecto.reino}',Phylum='{insecto.phyllum}',Clase='{insecto.clase}',Orden='{insecto.orden}',Familia='{insecto.familia}',Detalles='{insecto.informacion}', Taxa='{insecto.Taxa}',Morfo='{insecto.Morfoespecie}', Abun='{insecto.Abundancia}',Frasco='{insecto.NumFrasco}',Vegetacion='{insecto.TipoVegetacion}',Metodo='{insecto.MetodoColecta}',Sustrato='{insecto.Sustrato}' ,Localida='{insecto.Localidad}',Fecha='{f1}' ,Longitud='{insecto.Longitud}',Altitud='{insecto.Altitud}' Where Id='{insecto.Id}'";
+                                                                                                                                                                                                              
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                    connection.Close();
+                    connection.Dispose();
+                }
+            }
+            return RedirectToAction("List");
+        }
+
+        public IActionResult Delete(int id)
+        {
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            Insectos insec = new Insectos();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"Select * From Registro Where Id='{id}'";
+                SqlCommand command = new SqlCommand(sql, connection);
+                connection.Open();
+                using (SqlDataReader dataReader = command.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                    {
+                        insec.usuario = Convert.ToString(dataReader["Usuario"]);
+                        insec.Id = Convert.ToInt32(dataReader["Id"]);
+                        insec.reino = Convert.ToString(dataReader["Reino"]);
+                        insec.phyllum = Convert.ToString(dataReader["Phylum"]);
+                        insec.clase = Convert.ToString(dataReader["Clase"]);
+                        insec.orden = Convert.ToString(dataReader["Orden"]);
+                        insec.familia = Convert.ToString(dataReader["Familia"]);
+                        insec.informacion = Convert.ToString(dataReader["Detalles"]);
+                        insec.Taxa = Convert.ToString(dataReader["Taxa"]);
+                        insec.Morfoespecie = Convert.ToString(dataReader["Morfo"]);
+                        insec.Abundancia = Convert.ToString(dataReader["Abun"]);
+                        insec.NumFrasco = Convert.ToInt32(dataReader["Frasco"]);
+                        insec.TipoVegetacion = Convert.ToString(dataReader["Vegetacion"]);
+                        insec.MetodoColecta = Convert.ToString(dataReader["Metodo"]);
+                        insec.Sustrato = Convert.ToString(dataReader["Sustrato"]);
+                        insec.Localidad = Convert.ToString(dataReader["Localida"]);
+                        insec.Fecha = Convert.ToDateTime(dataReader["Fecha"].ToString());
+                        insec.Longitud = Convert.ToString(dataReader["Longitud"]);
+                        insec.Altitud = Convert.ToInt32(dataReader["Altitud"]);
+                    }
+                }
+                connection.Close();
+                connection.Dispose();
+            }
+            return View(insec);
+        }
+
+        [HttpPost]
+        [ActionName ("Delete")]
+        public IActionResult Delet(int id)
+        {
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"Delete From Registro Where Id='{id}'";
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    connection.Open();
+                    try
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                    catch (SqlException ex)
+                    {
+                        ViewBag.Result = "No se pudo eliminar el dato solicitado:" + ex.Message;
+                    }
+                    connection.Close();
+                    connection.Dispose();
+                }
+            }
+            return RedirectToAction("List");
+        }
+
+
+        public IActionResult Details(int id)
+        {
+            string connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+            Insectos insec = new Insectos();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string sql = $"Select * From Registro Where Id='{id}'";
+                SqlCommand command = new SqlCommand(sql, connection);
+                connection.Open();
+                using (SqlDataReader dataReader = command.ExecuteReader())
+                {
+                    while (dataReader.Read())
+                    {
+
+                        insec.usuario = Convert.ToString(dataReader["Usuario"]);
+                        insec.Id = Convert.ToInt32(dataReader["Id"]);
+                        insec.reino = Convert.ToString(dataReader["Reino"]);
+                        insec.phyllum = Convert.ToString(dataReader["Phylum"]);
+                        insec.clase = Convert.ToString(dataReader["Clase"]);
+                        insec.orden = Convert.ToString(dataReader["Orden"]);
+                        insec.familia = Convert.ToString(dataReader["Familia"]);
+                        insec.informacion = Convert.ToString(dataReader["Detalles"]);
+                        insec.Taxa = Convert.ToString(dataReader["Taxa"]);
+                        insec.Morfoespecie = Convert.ToString(dataReader["Morfo"]);
+                        insec.Abundancia = Convert.ToString(dataReader["Abun"]);
+                        insec.NumFrasco = Convert.ToInt32(dataReader["Frasco"]);
+                        insec.TipoVegetacion = Convert.ToString(dataReader["Vegetacion"]);
+                        insec.MetodoColecta = Convert.ToString(dataReader["Metodo"]);
+                        insec.Sustrato = Convert.ToString(dataReader["Sustrato"]);
+                        insec.Localidad = Convert.ToString(dataReader["Localida"]);
+                        insec.Fecha = Convert.ToDateTime(dataReader["Fecha"].ToString());
+                        insec.Longitud = Convert.ToString(dataReader["Longitud"]);
+                        insec.Altitud = Convert.ToInt32(dataReader["Altitud"]);
+                    }
+                }
+                connection.Close();
+                connection.Dispose();
+            }
+            return View(insec);
+        }
+
+
+        //Fin
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Biologia/Controllers/Insectos.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Biologia.Controllers
+{
+    public class Insectos
+    {
+        [Required(ErrorMessage = "Este Campo es obligatorio")]
+        [Display(Name = "Usuario")]
+        public String usuario { get; set; }
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Este Campo es obligatorio")]
+        [Display(Name = "Reino")]
+        public String reino { get; set; }
+        [Required(ErrorMessage = "Este Campo es obligatorio")]
+        [Display(Name = "Phyllum")]
+        public String phyllum { get; set; }
+        [Required(ErrorMessage = "Este Campo es obligatorio")]
+        [Display(Name = "Clase")]
+        public String clase { get; set; }
+        [Required(ErrorMessage = "Este Campo es obligatorio")]
+        [Display(Name = "Orden")]
+        public String orden { get; set; }
+        [Required(ErrorMessage = "Este Campo es obligatorio")]
+        [Display(Name = "Familia")]
+        public String familia { get; set; }
+     
+        [Required(ErrorMessage = "Este Campo es obligatorio")]
+        [Display(Name = "Nombre Cientifico")]
+        public String Taxa {get; set;}
+        [Required(ErrorMessage = "Este Campo es obligatorio")]
+        public String Morfoespecie {get; set;}
+        [Required(ErrorMessage = "Este Campo es obligatorio")]
+        public String Abundancia { get; set; }
+        [Required(ErrorMessage = "Este Campo es obligatorio")]
+        [Display (Name = "Numero de Frasco")]
+        public int NumFrasco {get; set; }
+        [Required(ErrorMessage = "Este Campo es obligatorio")]
+        [Display(Name = "Tipo de Vegetacion")]
+        public String TipoVegetacion { get; set; }
+        [Required(ErrorMessage = "Este Campo es obligatorio")]
+        [Display(Name = "Metodo de Colecta")]
+        public String MetodoColecta { get; set; }
+        [Required(ErrorMessage = "Este Campo es obligatorio")]
+        public String Sustrato { get; set; }
+        [Required(ErrorMessage = "Este Campo es obligatorio")]
+        public String Localidad { get; set; }
+        [Required(ErrorMessage = "Este Campo es obligatorio")]
+        [Display(Name = "Fecha de colecta")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Fecha { get; set; }
+        [Required(ErrorMessage = "Este Campo es obligatorio")]
+        //[RegularExpression("[0-5](°)[0-5](')[0-5](\") [N]")]
+        [Display(Name = "Coordenadas  (90° 19 20\" N 90° 19 10\" W) ") ]
+        public String Longitud { get; set; }
+        [Required(ErrorMessage = "Este Campo es obligatorio")]
+        //[RegularExpression("00°00'00\"")]
+        [Display(Name = "Altitud (msnm)")]
+        public int Altitud { get; set; }
+        [Required(ErrorMessage = "Este Campo es obligatorio")]
+[Display(Name = "Información adicional")]
+        public String informacion { get; set; }
+
+
+
+    }
+}
